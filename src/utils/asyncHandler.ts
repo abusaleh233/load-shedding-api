161e5c2797eec9b3d31f73b70b3ffcbe2583e7fd
abusaleh233 <abusaleh233@gmail.com>
2026-09-05 22:51:18 +0600

    asyncHansdler

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -2,10 +2,6 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 
 type AsyncFn = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
 
-/**
- * Wraps async controller functions so rejected promises are forwarded
- * to Express's error-handling middleware instead of crashing the process.
- */
 export const asyncHandler = (fn: AsyncFn): RequestHandler => {
   return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
